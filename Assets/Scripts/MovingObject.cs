b68252c3ff703159c5fce24e1c82172f09a8f25f
choijaewan <ssss3044@gmail.com>
2023-05-18 06:56:01 +0900

    Moving practice

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -4,39 +4,66 @@ using UnityEngine;
 
 public class MovingObject : MonoBehaviour
 {
-	public float speed;
-	private Vector3 vector;
+    public float speed;
+    private Vector3 vector;
     public float runSpeed;
     private float applyRunSpeed;
+    public int walkCount;
+    private int currentWalkCount;
+    private bool canMove = true;
+    private bool applyRunFlag = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator MoveCoroutine()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                applyRunSpeed = runSpeed;
-            }
-            else
-            {
-                applyRunSpeed = 0;
-            }
-                vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+            applyRunSpeed = runSpeed;
+            applyRunFlag = true;
+        }
+        else
+        {
+            applyRunSpeed = 0;
+            applyRunFlag = false;
+        }
 
+        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+        while (currentWalkCount < walkCount)
+        {
             if (vector.x != 0)
             {
-                transform.Translate(vector.x * speed, 0, 0);
+                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
             }
             else if (vector.y != 0)
             {
-                transform.Translate(0, vector.y * speed, 0);
+                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+            }
+            yield return new WaitForSeconds(0.01f);
+            if (applyRunFlag)
+            {
+                currentWalkCount++;
+            }
+            currentWalkCount++;
+        }
+
+        currentWalkCount = 0;
+        canMove = true;
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                canMove = false;
+                StartCoroutine(MoveCoroutine());
             }
         }
     }
